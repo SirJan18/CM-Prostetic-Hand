7e33d36c61d123a5dc30cdf90580c6137e323878
SirJan18 <SirJan18@users.noreply.github.com>
2019-06-01 23:18:14 -0400

    Update hand.ino
    include ir reciever

--- hand/hand.ino
+++ hand/hand.ino
@@ -1,19 +1,27 @@
-#include<Servo.h>
+#include <Servo.h>
+#include <IRremote.h>
+
+
 
 Servo finger1;
 Servo finger2;
 Servo finger3;
 Servo finger4;
 Servo finger5;
+init reciever = 6; //IR Reciever Signal Connection
+IRrecv irrecv(reciever);
+decode_results results;
+
 
 
 void setup() {
   // Attach Fingers to different plugs
-  finger1.attach();
-  finger2.attach();
-  finger3.attach();
-  finger4.attach();  
-  finger5.attach();
+  finger1.attach(9);
+  finger2.attach(10);
+  finger3.attach(11);
+  finger4.attach(12);  
+  finger5.attach(13);
+  irrecv.enableIRIn(); //Start IR Reciever
 }
 
 void loop() {
