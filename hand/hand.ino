de3b4945a33f74671bb74a9bf300a65a3544b0a2
SirJan18 <SirJan18@users.noreply.github.com>
2019-06-01 22:53:25 -0400

    Create hand.ino
    define servos but no mount point yet

---
+++ hand/hand.ino
@@ -0,0 +1,23 @@
+#include<Servo.h>
+
+Servo finger1;
+Servo finger2;
+Servo finger3;
+Servo finger4;
+Servo finger5;
+
+
+void setup() {
+  // Attach Fingers to different plugs
+  finger1.attach();
+  finger2.attach();
+  finger3.attach();
+  finger4.attach();  
+  finger5.attach();
+}
+
+void loop() {
+
+ 
+
+}
