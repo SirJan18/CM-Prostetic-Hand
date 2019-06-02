1115e03b54d11abdacc0596b7432e1a59096865e
SirJan18 <SirJan18@users.noreply.github.com>
2019-06-02 12:10:13 -0400

    Update hand.ino
    Comment out IR until solution is achieved

--- hand/hand.ino
+++ hand/hand.ino
@@ -8,23 +8,29 @@ Servo finger2;
 Servo finger3;
 Servo finger4;
 Servo finger5;
-init reciever = 6; //IR Reciever Signal Connection
-IRrecv irrecv(reciever);
-decode_results results;
+//IRremote reciever = 6; //IR Reciever Signal Connection
+//IRrecv irrecv(reciever);
+//decode_results results;
+long randNum;
+
 
 
 
 void setup() {
+  Serial.begin(9600);
   // Attach Fingers to different plugs
   finger1.attach(9);
   finger2.attach(10);
   finger3.attach(11);
   finger4.attach(12);  
   finger5.attach(13);
-  irrecv.enableIRIn(); //Start IR Reciever
+  //irrecv.enableIRIn(); //Start IR Reciever
 }
 
 void loop() {
+
+  randNum = random(0,2);
+  Serial.print(randNum);
 
  
 
