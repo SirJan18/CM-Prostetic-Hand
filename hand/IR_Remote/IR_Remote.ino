f77542334b5bb71ee23e2000bd016d3f5962adc3
SirJan18 <SirJan18@users.noreply.github.com>
2019-06-03 12:56:43 -0400

    Update IR_Remote.ino
    working

--- hand/IR_Remote/IR_Remote.ino
+++ hand/IR_Remote/IR_Remote.ino
@@ -1,14 +1,25 @@
 #include <IRremote.h>
 
-IRrecv irrecv(8);
+
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
 decode_results results;
+
+
 
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
+Serial.print("IR Reciever Button Decode");
+irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
 }
